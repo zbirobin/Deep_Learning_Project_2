2bcd53e8b989436d9a541ee6bedab133a86e3165
zbirobin <zbi.robin@gmail.com>
2020-05-10 23:40:39 +0200

    Add docstring for Modules and generate_data

--- Modules.py
+++ Modules.py
@@ -5,6 +5,8 @@ from functional import relu, drelu, tanh, dtanh, sigmoid, dsigmoid
 
 
 class Module(ABC):
+    """Abstract class representing a module of a Neural Network.
+    """
 
     @abstractmethod
     def forward(self, *input):
@@ -24,37 +26,69 @@ class Module(ABC):
 
 
 class Sequential(Module):
+    """Class representing a Neural Network which is a sequence of the modules given in the constructor.
+    """
 
     def __init__(self, *modules):
+        """Builds a Neural Network using the modules given.
+
+        :param modules: modules of the Neural Network
+        """
         self.modules = list(modules)
         self.name = " => ".join([m.name for m in modules])
 
     def forward(self, input):
+        """Computes the forward pass of the Neural Network for the given input of N samples.
+
+        :param input: Tensor (N x input_dim of the first module)
+        :return: Tensor output (N x output_dim of the last module)
+        """
         output = input.clone()
         for module in self.modules:
             output = module.forward(output)
         return output
 
     def backward(self, gradwrtoutput):
+        """Computes the backward pass of the Neural Network for the given gradient w.r.t the output.
+
+        :param gradwrtoutput: (N x output_dim of the last module)
+        :return: Tensor the gradients w.r.t the input (N x input_dim of the first module)
+        """
         grad = gradwrtoutput.clone()
         for module in self.modules[::-1]:
             grad = module.backward(grad)
         return grad
 
     def param(self):
+        """Returns the parameters of the modules of the Neural Network.
+
+        :return: the parameters of the modules in an array
+        """
         params = []
         for module in self.modules:
             params.append(module.param())
         return params
 
     def update_param(self, eta):
+        """Updates the parameters of the modules using the gradient descent rule with learning rate eta.
+
+        :param eta: learning rate
+        """
         for module in self.modules:
             module.update_param(eta)
 
 
 class Linear(Module):
+    """Class representing a linear fully connected layer of a Neural Network.
+    """
 
     def __init__(self, input_dim, output_dim):
+        """Builds a linear fully connected Layer of a Neural Network of input dimension input_dim and output dimension
+        output_dim.
+
+        :param input_dim: integer
+        :param output_dim: integer
+        """
         self.input_dim = input_dim
         self.output_dim = output_dim
         self.input = None
@@ -65,16 +99,27 @@ class Linear(Module):
         self.name = "Linear({}x{})".format(self.input_dim, self.output_dim)
 
     def __init_parameters(self):
-        """Initialize the weights and the bias of the layer.
-        :return: weights matrix, bias
+        """Initializes the weights and the bias of the layer using normal distributions.
+        :return: Tensors weights matrix (input_dim x output_dim), bias (output_dim)
         """
         return empty(self.input_dim, self.output_dim).normal_(), empty(self.output_dim).normal_()
 
     def forward(self, input):
+        """Computes the forward pass of the layer, i.e., XW + B (broadcasting on b).
+
+        :param input: Tensor (N x input_dim)
+        :return: Tensor output (N x output_dim)
+        """
         self.input = input
-        return input.matmul(self.weights).add(self.bias)  # XW + B (broadcasting on b)
+        return input.matmul(self.weights).add(self.bias)
 
     def backward(self, gradwrtoutput):
+        """Computes the backward pass of the layer, i.e., the gradients w.r.t. the input using the gradients w.r.t. the
+        output given.
+
+        :param gradwrtoutput: Tensor (N x output_dim)
+        :return: Tensor gradients (N x input_dim)
+        """
         if self.input is not None:
             self.bias_grad = gradwrtoutput.sum(0)  # Gradient of sum is the sum of gradient
             weights_grad = self.input.t().matmul(gradwrtoutput)
@@ -84,9 +129,17 @@ class Linear(Module):
             raise Exception("Forward must be called before backward for " + self.name)
 
     def param(self):
-        return [(self.weights, self.weights_grad), (self.bias, self.bias_grad)]
+        """Returns the parameters of the layer.
+
+        :return: [Tensor weights matrix (input_dim x output_dim), Tensor bias (output_dim)]
+        """
+        return [self.weights, self.bias]
 
     def update_param(self, eta):
+        """Updates the parameters of the layer using the gradient descent rule with learning rate eta.
+
+        :param eta: learning rate
+        """
         self.weights -= eta * self.weights_grad
         self.bias -= eta * self.bias_grad
 
@@ -94,72 +147,130 @@ class Linear(Module):
 # Activations functions modules
 
 class ReLU(Module):
+    """Class representing a ReLU module of a Neural Network.
+    """
 
     def __init__(self):
+        """Builds the ReLU module.
+        """
         self.input = None
         self.name = "ReLU"
 
     def forward(self, input):
+        """Computes the forward pass of the ReLU module for the given input.
+
+        :param input: Tensor
+        :return: Tensor with shape as input
+        """
         self.input = input.clone()
         return relu(self.input)
 
     def backward(self, gradwrtoutput):
+        """Computes the backward pass of the ReLU module, i.e., the gradients w.r.t. the input using the gradients
+        w.r.t. the output given.
+
+        :param gradwrtoutput: Tensor with shape as input
+        :return: Tensor with shape as input
+        """
         if self.input is not None:
             return gradwrtoutput.mul(drelu(self.input))
         else:
             raise Exception("Forward must be called before backward for " + self.name)
 
     def param(self):
-        print(self.name + " has no parameters")
+        """Returns None as ReLU has no parameter.
+
+        :return: None
+        """
         return None
 
     def update_param(self, eta):
+        """Does nothing as ReLU has no parameter."""
         pass
 
 
 class Tanh(Module):
+    """Class representing a Tanh module of a Neural Network.
+    """
 
     def __init__(self):
+        """Builds the Tanh module.
+        """
         self.input = None
         self.name = "Tanh"
 
     def forward(self, input):
+        """Computes the forward pass of the Tanh module for the given input.
+
+        :param input: Tensor
+        :return: Tensor with shape as input
+        """
         self.input = input.clone()
         return tanh(self.input)
 
     def backward(self, gradwrtoutput):
+        """Computes the backward pass of the Tanh module, i.e., the gradients w.r.t. the input using the gradients
+        w.r.t. the output given.
+
+        :param gradwrtoutput: Tensor with shape as input
+        :return: Tensor with shape as input
+        """
         if self.input is not None:
             return gradwrtoutput.mul(dtanh(self.input))
         else:
             raise Exception("Forward must be called before backward for " + self.name)
 
     def param(self):
-        print(self.name + " has no parameters")
+        """Returns None as Tanh has no parameter.
+
+        :return: None
+        """
         return None
 
     def update_param(self, eta):
+        """Does nothing as Tanh has no parameter."""
         pass
 
 
 class Sigmoid(Module):
+    """Class representing a sigmoid module of a Neural Network.
+    """
 
     def __init__(self):
+        """Builds the Sigmoid module.
+        """
         self.input = None
         self.name = "Sigmoid"
 
     def forward(self, input):
+        """Computes the forward pass of the Sigmoid module for the given input.
+
+        :param input: Tensor
+        :return: Tensor with shape as input
+        """
         self.input = input.clone()
         return sigmoid(self.input)
 
     def backward(self, gradwrtoutput):
+        """Computes the backward pass of the Sigmoid module, i.e., the gradients w.r.t. the input using the gradients
+        w.r.t. the output given.
+
+        :param gradwrtoutput: Tensor with shape as input
+        :return: Tensor with shape as input
+        """
         if self.input is not None:
             return gradwrtoutput.mul(dsigmoid(self.input))
         else:
             raise Exception("Forward must be called before backward for " + self.name)
 
     def param(self):
+        """Returns None as Sigmoid has no parameter.
+
+        :return: None
+        """
         print(self.name + " has no parameters")
         return None
 
     def update_param(self, eta):
+        """Does nothing as Sigmoid has no parameter."""
         pass
--- generate_data.py
+++ generate_data.py
@@ -3,22 +3,27 @@ from torch import empty
 
 
 def generate_quarter_disc_set(N):
-    """Generates a set of N sampled uniformly in [0, 1]^2, each with a
-       label 0 if outside the disk of radius 1/√2π and 1 inside. 
-       
-       Return 2-tuple: input, target
+    """Generates a set of N sampled uniformly in [0, 1]^2, each with
+    label 0 if outside the disk of radius 1/√2π and 1 inside.
+
+    :param N: number of samples
+    :return: Tensors input (N x 2), target (N)
     """
+
     input = empty(N, 2).uniform_(0, 1)
     target = input.pow(2).sum(1).sub(1 / math.sqrt(2 * math.pi)).sign().add(1).div(2).long()
     return input, target
 
 
 def generate_data(N, normalize=False):
-    """Generates a training and a test set of N sampled uniformly in [0, 1]^2, each with a
-       label 0 if outside the disk of radius 1/√2π and 1 inside. 
-       
-       Return 4-tuple: train_input, train_target, test_input, test_target
+    """Generates a training and a test set of N sampled uniformly in [0, 1]^2, each with
+    label 0 if outside the disk of radius 1/√2π and 1 inside.
+
+    :param N: number of samples
+    :param normalize: boolean, normalize the data if True
+    :return: Tensors train_input (N x 2), train_target (N), test_input (N x 2), test_target (N)
     """
+
     train_input, train_target = generate_quarter_disc_set(N)
     test_input, test_target = generate_quarter_disc_set(N)
     if normalize:
@@ -27,3 +32,4 @@ def generate_data(N, normalize=False):
         test_input.sub_(mean).div_(std)
     
     return train_input, train_target, test_input, test_target
+
