68ea49ff7a89ab4fdbf68421df1a1bbe8ee5274b
jalel <jalel.zghonda@epfl.ch>
2020-05-11 12:11:36 +0200

    minor documentation and refactoring

--- training.py
+++ training.py
@@ -4,17 +4,32 @@ from functional import lossMSE, dlossMSE, lossMAE, dlossMAE
 
 
 def one_hot_encode(target):
+    """
+    Encode the targets in 1-hot encoding
+    :param target: target array of 1s and 0s
+    :return: targets encoded in 1-hot
+    """
     ohe_target = empty(target.shape[0], 2)
     ohe_target[:, 0] = target == 0
     ohe_target[:, 1] = target == 1
     return ohe_target
 
 
-def train_model_SGD(model, train_input, train_target, nb_epoch=25, mini_batch_size=10):
+def train_model_SGD(model, train_input, train_target, nb_epoch=25, mini_batch_size=10, eta=1e-3):
+    """
+    Uses Stochastic gradient descent algorithm to train the model
+    :param model: the model to train
+    :param train_input: the input data for the model
+    :param train_target: the output data for the model
+    :param nb_epoch: the number of training epochs
+    :param mini_batch_size: the batch size to use for the training
+    :param eta: the learning rate of the stochastic gradient descent algorithm used
+    :return: None
+    """
     nb_train_samples = train_input.shape[0]
     ohe_train_target = one_hot_encode(train_target)
     mse = LossMSE()
-    eta = 1e-3
+
     nb_batch_per_epoch = nb_train_samples // mini_batch_size
     for epoch in range(nb_epoch):
         loss = 0
@@ -27,11 +42,18 @@ def train_model_SGD(model, train_input, train_target, nb_epoch=25, mini_batch_si
 
             model.update_param(eta)
 
-        mean_loss = loss/nb_batch_per_epoch
+        mean_loss = loss / nb_batch_per_epoch
         print(f"Epoch: {epoch}, Mean loss: {mean_loss}")
 
 
 def accuracy(model, input, target):
+    """
+    Computes the accuracy metric for the model using the a set of input/target pair
+    :param model: the model to test
+    :param input: the input data
+    :param target: the output data
+    :return: the accuracy of the model
+    """
     output = model.forward(input)
     return (output.argmax(1) == target).sum() / float(output.shape[0])
 
